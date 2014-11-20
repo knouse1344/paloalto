9feff1e6c5b0b05db02956519e25961127b43d8e
Bryan <bryan.knouse@gmail.com>
2014-11-20 09:37:54 -0500

    basic placeholder page up and running

--- public/assets/application-e818307e60423aa63044723b3c829dbc.js
+++ public/assets/application-e818307e60423aa63044723b3c829dbc.js
@@ -11531,6 +11531,10 @@ return jQuery;
   };
 
 }).call(this);
+(function() {
+
+
+}).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //

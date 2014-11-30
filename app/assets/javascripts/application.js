bb0d812ae6f42075dcc777543815cbf3b1f3ac0a
Bryan <bryan.knouse@gmail.com>
2014-11-30 12:20:32 -0500

    boom

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,14 +25,91 @@ $(document).ready(function() {
 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 		  if (target.length) {
 		    $('html,body').animate({
-		      scrollTop: target.offset().top - 100
+		      scrollTop: target.offset().top - 150
 		    }, 1000);
 		    return false;
 		  }
 		}
 	});
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Horizontal Scroll on hover
+	/////////////////////////////////////////////////////////////////////////////////////
+	var amount = '';
+
+	function loop (direction, element) {
+		var $scroller = element;
+
+		if (direction == 'left') {
+			loopleft()
+		} else if (direction == 'right') {
+			loopright()
+		} else {
+			$scroller.stop();
+		}
+
+		function loopleft(){
+			amount = '-=50';		
+		    $scroller.stop().animate({scrollLeft: amount}, 100, 'linear', loopleft);
+		} 
+
+		function loopright(){
+			amount = '+=50';
+		    $scroller.stop().animate({scrollLeft: amount}, 100, 'linear', loopright);
+		}
+	}
+
+	function stop(scrollfix){
+	    loop('', scrollfix)
+	}
+
+	$(".hover-left").hover(		
+		function () {
+			var scrollfix = $(this).siblings('.scrollfix');
+			loop('left', scrollfix); // Loop-fn on mouseenter, stop-fn on mouseleave
+		}, function() {
+			var scrollfix = $(this).siblings('.scrollfix');
+			stop(scrollfix);
+	});
+
+	// $(".hover-left").hover(loopleft, stop); // Loop-fn on mouseenter, stop-fn on mouseleave
+	$(".hover-right").hover(
+		function () {
+			var scrollfix = $(this).siblings('.scrollfix');
+			loop('right', scrollfix); // Loop-fn on mouseenter, stop-fn on mouseleave
+		}, function() {
+			var scrollfix = $(this).siblings('.scrollfix');
+			stop(scrollfix);
+	});
+
+	// var amount = '';
+
+	// function scroll() {
+	//     $('.scrollfix').animate({scrollLeft: amount}, 25, 'linear',function() {
+	//         if (amount != '') {
+	//             scroll();
+	//         }
+	//     });
+	// }
+
+	// $('.hover-left').hover(function() {
+	//     amount = '+=20';
+	//     scroll();
+	// }, function() {
+	//     amount = '';
+	// });
+	// $('.hover-right').hover(function() {
+	//     amount = '-=10';
+	//     scroll();
+	// }, function() {
+	//     amount = '';
+	// });
 	
-	$(".section").hover(function() {
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Click Functions
+	/////////////////////////////////////////////////////////////////////////////////////
+	$(".section").on("click", function() {
 		$("#index").toggleClass('hover');
 		if($( this ).hasClass('products')) {
 			$('#index').toggleClass('products-hover');
@@ -57,27 +134,37 @@ $(document).ready(function() {
 		};
 	});
 
-	$(".nav-list li").hover(function() {
+	$(".nav-list li a").on("click", function() {
 		$("#index").toggleClass('hover');
-		if($( this ).hasClass('products')) {
+		if($( this ).parent().hasClass('products')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('products-hover');
-		} else if($( this ).hasClass('solutions')) {
+		} else if($( this ).parent().hasClass('solutions')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('solutions-hover')
-		} else if($( this ).hasClass('services')) {
+		} else if($( this ).parent().hasClass('services')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('services-hover');			
-		} else if($( this ).hasClass('partners')) {
+		} else if($( this ).parent().hasClass('partners')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('partners-hover');			
-		} else if($( this ).hasClass('customers')) {
+		} else if($( this ).parent().hasClass('customers')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('customers-hover');			
-		} else if($( this ).hasClass('company')) {
+		} else if($( this ).parent().hasClass('company')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('company-hover');		
-		} else if($( this ).hasClass('careers')) {
+		} else if($( this ).parent().hasClass('careers')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('careers-hover');			
-		} else if($( this ).hasClass('resources')) {
+		} else if($( this ).parent().hasClass('resources')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('resources-hover');			
-		} else if($( this ).hasClass('support')) {
+		} else if($( this ).parent().hasClass('support')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('support-hover');		
-		} else if($( this ).hasClass('contact')) {
+		} else if($( this ).parent().hasClass('contact')) {
+			$('#index').removeClass();
 			$('#index').toggleClass('contact-hover');			
 		};
 	});
